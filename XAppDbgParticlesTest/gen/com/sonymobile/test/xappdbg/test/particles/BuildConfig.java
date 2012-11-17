b6b8afb4f770208b600fe91f3a2108baaa73b231
Pal Szasz <pal.szasz@sonymobile.com>
2012-11-17 10:58:59 +0100

    Cleanup

--- XAppDbgParticlesTest/gen/com/sonymobile/test/xappdbg/test/particles/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.sonymobile.test.xappdbg.test.particles;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
